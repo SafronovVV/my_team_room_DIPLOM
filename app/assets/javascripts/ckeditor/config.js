2da961de64d48dd4d9f713906ebc7f01774da614
Safronov Vladislav <safronov.zapas@gmail.com>
2018-06-02 10:14:35 +0300

    Add ckeditor custom toolbar

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,9 @@
+CKEDITOR.editorConfig = (config) => {
+  config.toolbar = 'documentBasic';
+  config.toolbar_documentBasic = [
+    [ 'Bold','Italic','Underline','Strike', 'Blockquote' ],
+    [ 'Cut','Copy' ],
+    [ 'Styles','Format','FontSize' ],
+    [ 'NumberedList','BulletedList' ]
+  ]
+}
